2dada55e43de686cf358cc289b3467ee2b86a850
solo-mizan <mizanbd.eee@gmail.com>
2024-05-11 16:08:47 +0600

    chore: landing page happy customers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,102 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import { StarFilledIcon } from "@radix-ui/react-icons";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="lg:gap-x-0 xl:gap-x-8 lg:grid lg:grid-cols-3 pt-10 lg:pt-24 xl:pt-32 pb-24 sm:pb-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative flex flex-col items-center lg:items-start mx-auto text-center lg:text-left">
+              <div className="lg:block -top-20 left-0 absolute hidden w-28">
+                <img src="/snake-1.png" alt="logo"/>
+              </div>
+              <h1
+                className="relative mt-16 w-fit font-bold text-5xl text-balance text-gray-900 md:text-6xl lg:text-7xl !leading-tight tracking-tight"
+              >Find your <span
+                  className="bg-green-600 px-2 text-white"
+                >soulmate </span>
+                within our trusted network
+              </h1>
+              <p className="mt-8 lg:pr-10 max-w-prose text-balance text-center text-lg md:text-wrap lg:text-left">Don&apos;t settle for just anyone. Find someone who truly understand and cherishes you. Let <span className="bg-slate-500 bg-slate-700 px-1 font-semibold text-white">Duo</span> guide you on your journey to love.</p>
+              <ul className="flex flex-col items-center sm:items-start space-y-2 mt-8 font-medium text-left">
+                <div className="space-y-2">
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="w-5 h-5 text-green-600 shrink-0" />
+                    Free to join and create a profile
+                  </li>          
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="w-5 h-5 text-green-600 shrink-0" />
+                    Advanced search filters to find compatible matches.
+                  </li>          
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="w-5 h-5 text-green-600 shrink-0" />
+                    Detailed profiles to showcase your personality and interests
+                  </li>          
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="w-5 h-5 text-green-600 shrink-0" />
+                    Verified profiles for added peace of mind
+                  </li>          
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="w-5 h-5 text-green-600 shrink-0" />
+                    Positive and inclusive environment for all singles
+                  </li>          
+                </div>
+              </ul>
+              <div className="flex sm:flex-row flex-col items-center sm:items-start gap-5 mt-12">
+                <div className="flex -space-x-3">
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block rounded-full w-10 h-10 ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block rounded-full w-10 h-10 ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block rounded-full w-10 h-10 ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block rounded-full w-10 h-10 ring-2 ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="user image"
+                  />
+                  <Image
+                    width={100}
+                    height={100}
+                    className="inline-block rounded-full w-10 h-10 object-cover ring-2 ring-slate-100"
+                    src="/users/user-5.jpg"
+                    alt="user image"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="w-4 h-4 text-green-600 fill-green-500"/>
+                    <Star className="w-4 h-4 text-green-600 fill-green-500"/>
+                    <Star className="w-4 h-4 text-green-600 fill-green-500"/>
+                    <Star className="w-4 h-4 text-green-600 fill-green-500"/>
+                    <Star className="w-4 h-4 text-green-600 fill-green-500"/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </div>
   );
 }
